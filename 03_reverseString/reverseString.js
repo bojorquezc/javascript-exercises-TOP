2d89234d01cb6319cb42a6b361f6f53f4487f83c
Conrado <42232126+bojorquezc@users.noreply.github.com>
2022-12-10 12:03:20 -0800

    reverseString Jest excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringToArray = string.split("");
+    const reversedArray = stringToArray.reverse();
+    const arrayToString = reversedArray.join("");
+    return arrayToString;
 };
 
 // Do not edit below this line
