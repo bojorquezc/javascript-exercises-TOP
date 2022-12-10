c60bbbd2a1ab51ef432caf35262f4870b8689af8
Conrado <42232126+bojorquezc@users.noreply.github.com>
2022-12-09 23:20:39 -0800

    repeatString Jest excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeated = '';
+    if(num < 0) {
+        return 'ERROR';
+    }
+    for(let i = 1; i <= num; i++) {
+        repeated += string.toString(); 
+    }
+    return repeated;
 };
 
 // Do not edit below this line
