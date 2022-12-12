f672f29db93b51c423ffb6cd7cc5e111f211bca9
Conrado <42232126+bojorquezc@users.noreply.github.com>
2022-12-11 19:34:55 -0800

    Complete removeFromArray Jest excercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,14 @@
 const removeFromArray = function(toArray, ...toRemove) {
     const loadedArray = Array.from(toArray);
 
-    if (toRemove.length == 1) {
-        const indexToRemove = loadedArray.indexOf(toRemove);
-        loadedArray.splice(indexToRemove, 1);
-    } else if (toRemove.length > 1) {
-        for (let i = 1; i < toRemove.length; i++) {
-            for(const item of toRemove) {
-                const indexToRemove = loadedArray.indexOf(item);
-                loadedArray.splice(indexToRemove, 1);
-            }
+    for(const item of toRemove) {
+        if (loadedArray.indexOf(item) === -1) {
+            continue;
         }
+        const indexToRemove = loadedArray.indexOf(item);
+        loadedArray.splice(indexToRemove, 1);
     }
     return loadedArray;
-    //console.log(loadedArray);
 }
 // Do not edit below this line
 module.exports = removeFromArray;
