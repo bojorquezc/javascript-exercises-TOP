c7b7bf732ef77daad254cb246e038f860066b78b
Conrado <42232126+bojorquezc@users.noreply.github.com>
2022-12-12 14:58:16 -0800

    tempConversion Jest excercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrTemp) {
+  let celsiusTemp = 0;
+  let roundCelsiusTemp = 0;
+  celsiusTemp = (fahrTemp - 32) * (5/9);
+  roundCelsiusTemp = Math.round(celsiusTemp * 10) / 10;
+  return roundCelsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(celsTemp) {
+  let fahrenheitTemp = 0;
+  let roundFahrenheitTemp = 0;
+  fahrenheitTemp = ((celsTemp * (9/5)) + 32);
+  roundFahrenheitTemp = Math.round(fahrenheitTemp * 10) / 10;
+  return roundFahrenheitTemp;
 };
 
 // Do not edit below this line
