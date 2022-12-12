f3e258a7e3de61c009233f67a7e84268b1cefcb2
Conrado <42232126+bojorquezc@users.noreply.github.com>
2022-12-11 22:38:52 -0800

    sumAll Jest excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function (minNumber, maxNumber) {
+    const numberArray = [];
 
+    // Inverse the values to add to array if minNumber is greater than maxNumber
+    if (minNumber > maxNumber) {
+        for (let i = maxNumber; i <= minNumber; i++) {
+            numberArray.push(i);
+        }
+    }
+    // Check if values are not negative
+    if (minNumber < 0 || maxNumber < 0) {
+        return 'ERROR';
+    }
+
+    // Check if argument is a number
+    if (typeof (minNumber) != 'number' || typeof (maxNumber) != 'number') {
+        return 'ERROR';
+    }
+
+    // Create array from min and max numbers
+    for (let i = minNumber; i <= maxNumber; i++) {
+        numberArray.push(i);
+    }
+
+    // Iterate and sum the array values
+    let sumArray = 0;
+    for (let index = 0; index < numberArray.length; index++) {
+        sumArray += Number(numberArray[index]);
+    }
+    return sumArray;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
