810a1ef16b1741c213dccf09cd982dabfe8463eb
Conrado Bojorquez <42232126+bojorquezc@users.noreply.github.com>
2022-12-29 15:40:54 -0800

    Complete palindromes Jest excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    reversedPhrase = phrase.split('').reverse().join('');
+    reversedLower = reversedPhrase.toLowerCase().replace(/[^a-zA-Z]/g, '');
+    regularPhraseLower = phrase.toLowerCase().replace(/[^a-zA-Z]/g, '');
 
+    if (regularPhraseLower == reversedLower) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
