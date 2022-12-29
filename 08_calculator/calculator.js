43f0b2942e901f805c64f2369e2efade26e7b556
Conrado Bojorquez <42232126+bojorquezc@users.noreply.github.com>
2022-12-28 22:26:54 -0800

    Complete calculator Jest excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(array) {
+  let arr = array;
+  return arr.reduce((sum, current) => sum + current, 0);
 	
 };
 
-const subtract = function() {
+const multiply = function(array) {
+  let arr = array;
+  return arr.reduce((product, current) => product * current, 1);
+};
+
+const power = function(a, b) {
+  return a**b;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  fact = a;
+  if (a == 0) {
+    return 1;
+  } else {
+    for (let i = 1; i < a; i++) {
+      fact *= i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
