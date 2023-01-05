76620662eb647bedfbbb43d296a5b90004d2ab7a
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-01-04 19:58:12 -0800

    fibonacci Jest excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let a = 1; 
+    let b = 1; 
+    let c = 0;
+    let indexNumber = Number(index);
 
+    if (indexNumber == 1 || indexNumber == 2) {
+        return 1;
+    } else if (indexNumber == 0) {
+        return 0;
+    } else if (indexNumber < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 0; i < indexNumber-2; i++) {
+            c = a + b;
+            b = a;
+            a = c;
+        }
+    }
+    return c;
 };
 
 // Do not edit below this line
